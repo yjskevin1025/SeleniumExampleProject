d8fbb9a09f7266a2f07656ef4118f3801338cbc5
Main <yjs1357948@gmail.com>
2020-04-10 19:52:05 +0900

    ??

--- pythonTest.py
+++ pythonTest.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 
-driver = webdriver.Chrome()
+driver = webdriver.Chrome('C:\\Users\\Main\\Desktop\\크롤링\\driver\\chromedriver.exe')
 
-driver.close()
+driver.implicitly_wait(3)
+driver.get('https://nid.naver.com/nidlogin.login')
